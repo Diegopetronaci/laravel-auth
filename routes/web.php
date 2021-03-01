adb498ff497dbf52ab2799d24567a734f41ae75a
Diego <Petronacidiego@gmail.com>
2021-03-01 22:36:15 +0100

    esercizio base completato
    da implemetare edit, delete, aggiungere collegamenti link a pagine create, e reindirizzare alla al post view cliccando dal blog

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'PageController@index')->name('homepage');
 Route::get('about', 'PageController@about')->name('about');
 Route::get('contacts', 'PageController@contacts')->name('contacts');
+Route::get('blog', 'PostController@index')->name('blog');
+Route::get('blog/{post}', 'PostController@show');
+
+
 
 
 Auth::routes(['register' => false]);
